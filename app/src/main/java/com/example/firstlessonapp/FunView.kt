4622af13247297019145f013c0ce9f9569bd1c10
Ashot-26 <ashotbarsegyan14@gmail.com>
2025-02-13 00:19:29 +0400

    2st commit

--- app/src/main/java/com/example/firstlessonapp/FunView.kt
+++ app/src/main/java/com/example/firstlessonapp/FunView.kt
@@ -15,6 +15,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
+
 class CountViewModel : ViewModel() {
     private val _count = MutableLiveData(0)
     val count: LiveData<Int> = _count
