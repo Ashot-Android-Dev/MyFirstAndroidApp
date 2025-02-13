bc8dd63f4030fc6ca5939231ef97c6b94da727a3
Ashot-Android-Dev <ashotb750@gmail.com>
2025-02-13 14:28:54 +0400

    plus imports

--- app/src/main/java/com/example/firstlessonapp/MainActivity.kt
+++ app/src/main/java/com/example/firstlessonapp/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.firstlessonapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.ui.res.stringResource
+import com.example.firstlessonapp.passwordVerification.ApplicationPhoneFun
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,16 +23,16 @@ class MainActivity : ComponentActivity() {
 //                    users.forEach { println("user${it.name} ${it.surname}") }
 //                }
 //            }
-//            ApplicationPhoneFun(
-//                titleWithText = stringResource(R.string.title_text),
-//                infoWithText = stringResource(R.string.info_text),
-//                genderWithText = stringResource(R.string.gender_info_text),
-//                birthInfoWithText = stringResource(R.string.birth_info_text),
-//                textForWeight = "" ,
-//                textForHeight = "",
-//                firstButtonWithText = stringResource(R.string.text_with_button_first),
-//                secondButtonWithText = stringResource(R.string.text_with_button_second)
-//            )
+            ApplicationPhoneFun(
+                titleWithText = stringResource(R.string.title_text),
+                infoWithText = stringResource(R.string.info_text),
+                genderWithText = stringResource(R.string.gender_info_text),
+                birthInfoWithText = stringResource(R.string.birth_info_text),
+                textForWeight = "" ,
+                textForHeight = "",
+                firstButtonWithText = stringResource(R.string.text_with_button_first),
+                secondButtonWithText = stringResource(R.string.text_with_button_second)
+            )
 
         }
     }
