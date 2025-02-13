d5365e99986426c816bee9dc0444cfd11c084aaa
Ashot-Android-Dev <ashotb750@gmail.com>
2025-02-13 14:22:50 +0400

    minus imports

--- app/src/main/java/com/example/firstlessonapp/MainActivity.kt
+++ app/src/main/java/com/example/firstlessonapp/MainActivity.kt
@@ -3,7 +3,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 class MainActivity : ComponentActivity() {
-//    private lateinit var carSaleDatabase: CarSaleDb
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
